f4e7bc0bdebae77d96b009fff7e0fcff85ef1214
Eason Nie <nyixin318@gmail.com>
2016-07-05 17:49:29 -0500

    Test for filter maxlength.

---
+++ try-tensorflow/testscript/test-filter.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+
+input = np.random.randint(0, 10, (4, 10))
+
+print(input)
+
+input_x = tf.placeholder(dtype=tf.int32, shape=[None, 10])
+
+length = 5
+
+maxlen = tf.cast(tf.fill([tf.shape(input_x)[0], 10], length), tf.int32)
+filter = tf.less_equal(input_x, maxlen)
+po_filter = tf.select(filter, input_x, maxlen)
+
+with tf.Session() as sess:
+    _, d_po_filter = sess.run((filter, po_filter), feed_dict={input_x: input})
+    print(d_po_filter)
