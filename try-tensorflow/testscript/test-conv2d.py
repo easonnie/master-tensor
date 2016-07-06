cc9074d6e77e0ebc650f263a27604d0422f0169c
Eason Nie <nyixin318@gmail.com>
2016-07-06 15:01:02 -0500

    Test for tf.nn.conv2d.

--- try-tensorflow/testscript/test-conv2d.py
+++ try-tensorflow/testscript/test-conv2d.py
@@ -1,12 +1,12 @@
 import numpy as np
 import tensorflow as tf
 
-batch_size = 3
+batch_size = 1
 max_seq_len = 5
 word_d = 4
 
 input = tf.Variable(tf.random_uniform([batch_size, max_seq_len, word_d], 0, 10, dtype=tf.int32, seed=10))
-tran_input = tf.reshape(input, [batch_size, 1, max_seq_len, word_d])
+tran_input = tf.reshape(input, [-1, 1, max_seq_len, word_d])
 # batch_size
 
 n_gram = 4
@@ -16,7 +16,7 @@ filter = tf.Variable(tf.random_uniform([1, n_gram, word_d, n_feature], 0, 5, dty
 
 result = tf.nn.conv2d(tf.to_float(tran_input), tf.to_float(filter), strides=[1, 1, 1, 1], padding='SAME')
 
-tran_result = tf.reshape(result, [batch_size, max_seq_len, n_feature])
+tran_result = tf.reshape(result, [-1, max_seq_len, n_feature])
 
 init_op = tf.initialize_all_variables()
 
